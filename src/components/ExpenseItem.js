8edf7376e6ae2452d37b55796fa5ff5238ea3683
Breno A.B <77648761+brenob21@users.noreply.github.com>
2024-04-04 09:34:50 -0300

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import '../components.css'
 
 const ExpenseItem = (props) => {
     const { currency, dispatch } = useContext(AppContext);
@@ -25,11 +26,24 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button className='btn btn-add' onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button className='btn btn-rmv' onClick={event=> decreaseAllocation(props.name)}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
